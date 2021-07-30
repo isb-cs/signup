8bc7451d8cd2cae5a3b1932ab8d60e9107a2fc47
isb-test1 <raman.gandhi@outlook.com>
2021-07-30 22:10:07 +0200

    this is the commit for rg_helloworld feature

---
+++ rg_helloworld.py
@@ -0,0 +1,9 @@
+# Write a program below which prints 'Hello *name*'.
+# The specifications are:
+# Ask the user for their name and assign it to a variable.
+# Print the message 'Hello *user's name*'
+#
+name = input("Enter your name: ")
+print("hello ", name)
+for i in range (3):
+    print("hello again")
